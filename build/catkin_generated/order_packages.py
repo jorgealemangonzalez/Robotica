92d121c11e35e555b147728e6b095c35c88aa098
u114139 <u114139@54030p07u.ds.upf.edu>
2016-10-26 13:07:46 +0200

    cambiada la estructura

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/jorge/Escritorio/Clase/Robotica/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/jorge/Escritorio/Clase/Robotica/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/jorge/Escritorio/Clase/Robotica/catkin_ws/devel;/opt/ros/indigo" != "" else []
--- build/turtlesim_ex1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jorge/Escritorio/Clase/Robotica/catkin_ws/src/turtlesim_ex1
-# Build directory: /home/jorge/Escritorio/Clase/Robotica/catkin_ws/build/turtlesim_ex1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
