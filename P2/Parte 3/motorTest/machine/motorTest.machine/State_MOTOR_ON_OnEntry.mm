763e37ff947cfc80ae53fcbf888b06e3da56a4f1
Mik-C <miquelferran.floriach01@estudiant.upf.edu>
2016-11-04 15:01:34 +0100

    Proportional Control terminado

--- P2/Parte 3/motorTest/machine/motorTest.machine/State_MOTOR_ON_OnEntry.mm
+++ P2/Parte 3/motorTest/machine/motorTest.machine/State_MOTOR_ON_OnEntry.mm
@@ -1,6 +1,8 @@
 ros_webots_epuck_nxt_differential_robot::Rrobot msg;
-msg.leftMotor.power= 50;
-msg.rightMotor.power= 50;
+actualDistance = (actualDistance > 100+distanceToKeep ? 100+distanceToKeep : actualDistance);
+msg.leftMotor.power= (int) (actualDistance - distanceToKeep) ;
+msg.rightMotor.power=  (int) (actualDistance - distanceToKeep);
+std::cout<<"Power front :"<<msg.leftMotor.power <<"actualDist: "<<actualDistance<<"distToKeep: "<<distanceToKeep<<std::endl; 
 msg.leftButtonSwitch.on=true;
 msg.rightButtonSwitch.on=true;
 robot_pub.publish(msg);
